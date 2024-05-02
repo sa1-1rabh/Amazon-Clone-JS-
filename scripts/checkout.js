696d6aef000b39c44b65f6d98f84b120ddeadfc0
EXPERTGMR <111639654+EXPERTGMR@users.noreply.github.com>
2024-05-02 16:57:08 +0530

    created OOP version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderCartSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import "../data/cart-oop.js";
 
 renderCartSummary();
 
